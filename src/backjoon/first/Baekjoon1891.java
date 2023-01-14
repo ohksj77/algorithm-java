8e20ab508413963a3966104e21dd91603adc43bc
ohksj77 <ohksj77@gmail.com>
2023-01-14 19:06:18 +0900

    2023-1-14 백준 문제풀이

--- src/backjoon/first/Baekjoon1891.java
+++ src/backjoon/first/Baekjoon1891.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Baekjoon1891 {
 
     private static int d;
-    private static long num;
+    private static String num;
     private static Element dxdy, nxny;
     private static String answer = "";
 
@@ -16,7 +16,7 @@ public class Baekjoon1891 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         d = Integer.parseInt(st.nextToken());
-        num = Long.parseLong(st.nextToken());
+        num = st.nextToken();
         st = new StringTokenizer(br.readLine());
         long x = Long.parseLong(st.nextToken());
         long y = Long.parseLong(st.nextToken());
@@ -35,35 +35,34 @@ public class Baekjoon1891 {
 
     }
 
-    private static void check(long n1, long n2, long m1, long m2) {
-        if (answer.length() == d) return;
+    private static String check(long n1, long n2, long m1, long m2) {
+        if (answer.length() == d) return answer;
         if (n1 <= nxny.x && nxny.x < (n1 + n2) / 2 && (m1 + m2) / 2 <= nxny.y && nxny.y < m2) {
             answer += "1";
-            check(n1, (n1 + n2) / 2, (m1 + m2) / 2, m2);
+            return check(n1, (n1 + n2) / 2, (m1 + m2) / 2, m2);
         } else if (n1 <= nxny.x && nxny.x < (n1 + n2) / 2 && m1 <= nxny.y && nxny.y < (m1 + m2) / 2) {
             answer += "2";
-            check(n1, (n1 + n2) / 2, m1, (m1 + m2) / 2);
+            return check(n1, (n1 + n2) / 2, m1, (m1 + m2) / 2);
         } else if ((n1 + n2) / 2 <= nxny.x && nxny.x < n2 && m1 <= nxny.y && nxny.y < (m1+m2) / 2) {
             answer += "3";
-            check((n1 + n2) / 2, n2, m1, (m1 + m2) / 2);
-        } else if ((n1 + n2) / 2 <= nxny.x && nxny.y < n2 && (m1 + m2) / 2 <= nxny.x && nxny.y < m2) {
+            return check((n1 + n2) / 2, n2, m1, (m1 + m2) / 2);
+        } else {
             answer += "4";
-            check((n1 + n2) / 2, n2, (m1 + m2) / 2, m2);
+            return check((n1 + n2) / 2, n2, (m1 + m2) / 2, m2);
         }
     }
 
     private static void find(long n1, long n2, long m1, long m2, int index) {
-        String number = String.valueOf(num);
         if (index == d) {
             dxdy = new Element(n1, m1);
             return;
         }
-        int num = number.charAt(Integer.parseInt(String.valueOf(index))) - '0';
+        int number = num.charAt(Integer.parseInt(String.valueOf(index))) - '0';
 
-        if (num == 1) find(n1, (n1 + n2) / 2, (m1 + m2) / 2, m2, index + 1);
-        else if (num == 2) find(n1, (n1 + n2) / 2, m1, (m1 + m2) / 2, index + 1);
-        else if (num == 3) find((n1 + n2) / 2, n2, m1, (m1 + m2) / 2, index + 1);
-        else if (num == 4) find((n1 + n2) / 2, n2, (m1 + m2) / 2, m2, index + 1);
+        if (number == 1) find(n1, (n1 + n2) / 2, (m1 + m2) / 2, m2, index + 1);
+        else if (number == 2) find(n1, (n1 + n2) / 2, m1, (m1 + m2) / 2, index + 1);
+        else if (number == 3) find((n1 + n2) / 2, n2, m1, (m1 + m2) / 2, index + 1);
+        else if (number == 4) find((n1 + n2) / 2, n2, (m1 + m2) / 2, m2, index + 1);
     }
 
     private static class Element {
