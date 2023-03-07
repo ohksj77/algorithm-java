20ac19d45a76553d81e34bd4cf887f9046e19434
ohksj77 <ohksj77@gmail.com>
2023-03-07 16:04:13 +0900

    2023-3-8 백준 문제풀이 3

---
+++ src/backjoon/first/Baekjoon11625.java
@@ -0,0 +1,35 @@
+package backjoon.first;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon11625 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Map<Long, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            Long cardNum = Long.parseLong(br.readLine());
+            map.put(cardNum, map.getOrDefault(cardNum, 0) + 1);
+        }
+        int maxValue = 0;
+        long answer = 0L;
+
+        for (long key : map.keySet()) {
+            int value = map.get(key);
+            if (maxValue < value) {
+                maxValue = value;
+                answer = key;
+            } else if (value == maxValue) {
+                answer = Math.min(answer, key);
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+    }
+}
