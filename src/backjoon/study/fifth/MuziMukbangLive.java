6e9a054cb74a3135f28994cfae13a154ba251678
ohksj77 <ohksj77@gmail.com>
2023-05-10 00:15:51 +0900

    2023-5-10 알고리즘 문제풀이 1

--- src/backjoon/study/fifth/MuziMukbangLive.java
+++ src/backjoon/study/fifth/MuziMukbangLive.java
@@ -1,29 +1,49 @@
 package backjoon.study.fifth;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
 
-public class MuziMukbang {
+public class MuziMukbangLive {
 
     public static void main(String[] args) {
         int answer = solution(new int[]{3, 1, 2}, 5);
         System.out.println(answer);
     }
     public static int solution(int[] food_times, long k) {
-        int count = 0;
-        int sum = Arrays.stream(food_times).sum();
-        while (count <= sum) {
-            for (int i = 0; i < food_times.length; i++) {
-                if (count == k) {
-                    return i + 1;
-                }
-                if (food_times[i] != 0) {
-                    food_times[i]--;
+        LinkedList<Food> list = new LinkedList<>();
+        int len = food_times.length;
+        for(int i = 0; i < len; i++) {
+            list.add(new Food(i + 1, food_times[i]));
+        }
+        Collections.sort(list, (o1, o2) -> o1.time - o2.time);
+
+        int current_time = 0;
+        int idx = 0;
+        for(Food f : list) {
+            long diff = f.time - current_time;
+            if(diff != 0) {
+                long spend = diff * len;
+                if(spend <= k) {
+                    k -= spend;
+                    current_time = f.time;
                 } else {
-                    continue;
+                    k %= len;
+                    list.subList(idx, food_times.length).sort((o1, o2) -> o1.num - o2.num);
+                    return list.get(idx + (int)k).num;
                 }
-                count++;
             }
+            idx++;
+            len--;
         }
         return -1;
+    }
+
+    static class Food {
+        int num;
+        int time;
+        Food(int num, int time) {
+            this.num = num;
+            this.time = time;
+        }
     }
 }
