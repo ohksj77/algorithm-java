576ad6a6ff070fac97379ac085ed149c810f2ab5
ohksj77 <ohksj77@gmail.com>
2023-05-18 12:35:31 +0900

    2023-5-18 알고리즘 문제풀이 3

---
+++ src/backjoon/study/sixth/Baekjoon1715.java
@@ -0,0 +1,29 @@
+package backjoon.study.sixth;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Baekjoon1715 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int sum = 0;
+
+        while (pq.size() > 1) {
+            int tmp = pq.poll() + pq.poll();
+            sum += tmp;
+            pq.add(tmp);
+        }
+
+        bw.write(Integer.toString(sum));
+        bw.flush();
+    }
+}
