cf271eceb6c357ab89039aed9991b50f0d6bafa3
ohksj77 <ohksj77@gmail.com>
2023-05-24 07:54:12 +0900

    2023-5-24 알고리즘 문제풀이

---
+++ src/backjoon/study/seventh/Baekjoon14501.java
@@ -0,0 +1,34 @@
+package backjoon.study.seventh;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon14501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] t = new int[n];
+        int[] p = new int[n];
+
+        StringTokenizer st;
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+
+        for (int i=0; i<n; i++) {
+            if (i + t[i] <= n) {
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+        bw.write(Integer.toString(dp[n]));
+        bw.flush();
+    }
+}
