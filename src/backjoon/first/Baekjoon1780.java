9b2497cc0aef60597153393449c6590f5a5b46d8
김승진 <ohksj77@gmail.com>
2023-07-04 13:42:56 +0900

    test

--- src/backjoon/first/Baekjoon1780.java
+++ src/backjoon/first/Baekjoon1780.java
@@ -1,6 +1,8 @@
 package backjoon.first;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon1780 {
