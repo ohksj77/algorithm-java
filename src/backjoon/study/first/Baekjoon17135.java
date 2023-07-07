88842827cb48c639fc5d3cd3d08ed4b1091fe025
김승진 <ohksj77@gmail.com>
2023-07-07 18:57:58 +0900

    test.....

--- src/backjoon/study/first/Baekjoon17135.java
+++ src/backjoon/study/first/Baekjoon17135.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Baekjoon17135 {
     private static int n, m, d;
