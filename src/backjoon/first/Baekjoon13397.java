3b1a3efc6c0361c829e03c71e74b798437d25b24
김승진 <ohksj77@gmail.com>
2024-01-15 03:11:11 +0900

    2024-01-15 알고리즘 문제 풀이 2

---
+++ src/backjoon/first/Baekjoon13397.java
@@ -0,0 +1,65 @@
+package backjoon.first;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Baekjoon13397 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] values = new int[n];
+
+        int left = 0;
+        int right = Integer.MIN_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            values[i] = value;
+            right = Math.max(value, right);
+        }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            int count = getCount(n, values, mid);
+
+            if (count > m) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        bw.write(Integer.toString(right));
+        bw.flush();
+    }
+
+    private static int getCount(int n, int[] arr, int mid) {
+        int count = 1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+            if (max - min > mid) {
+                count++;
+                max = Integer.MIN_VALUE;
+                min = Integer.MAX_VALUE;
+                i--;
+            }
+        }
+        return count;
+    }
+}
