cbca966d10cdfa790aef121d38869e23bd69cd01
김승진 <ohksj77@gmail.com>
2024-01-14 18:36:18 +0900

    2023-01-14 알고리즘 문제 풀이 1

---
+++ src/backjoon/leetcode/DailyTemperatures.java
@@ -0,0 +1,24 @@
+package backjoon.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] result = new int[temperatures.length];
+        Deque<int[]> stack = new ArrayDeque<>(temperatures.length);
+
+        for (int day = 0; day < temperatures.length; day++) {
+            final int temperature = temperatures[day];
+            while (!stack.isEmpty() && temperatures[stack.peek()[1]] < temperature) {
+                int index = stack.pop()[0];
+                result[index] = day - index;
+            }
+            stack.push(new int[] {day, temperature});
+        }
+
+        return result;
+    }
+}
---
+++ src/backjoon/leetcode/KeysAndRooms2.java
@@ -0,0 +1,32 @@
+package backjoon.leetcode;
+
+import java.util.List;
+
+public class KeysAndRooms2 {
+
+    private boolean[] visited;
+    private List<List<Integer>> rooms;
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        visited = new boolean[rooms.size()];
+        this.rooms = rooms;
+
+        dfs(0);
+
+        for (boolean visit : visited) {
+            if (!visit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void dfs(int index) {
+        visited[index] = true;
+        for (Integer key : rooms.get(index)) {
+            if (!visited[key]) {
+                dfs(key);
+            }
+        }
+    }
+}
