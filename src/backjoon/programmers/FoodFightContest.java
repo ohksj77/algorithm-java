7ad799156891acfc3e550e33dff80daae3374b9d
김승진 <ohksj77@gmail.com>
2024-05-11 13:02:39 +0900

    2024-05-11 알고리즘 문제 풀이

---
+++ src/backjoon/programmers/FoodFightContest.java
@@ -0,0 +1,18 @@
+package backjoon.programmers;
+
+public class FoodFightContest {
+
+    public String solution(int[] food) {
+        final StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < food.length; i++) {
+            final int f = food[i];
+            sb.append(String.valueOf(i).repeat((int) (f / 2)));
+        }
+
+        final String left = sb.toString();
+        final String right = sb.reverse().toString();
+
+        return left + "0".repeat(food[0]) + right;
+    }
+}
