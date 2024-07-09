7f841f765b76a5339caa1648ee50593cc84dac31
김승진 <ohksj77@gmail.com>
2024-07-09 13:17:51 +0900

    2024-07-09 알고리즘 문제 풀이 1

---
+++ src/algorithm/elice_challenge/정리정돈을_좋아하는_K씨.java
@@ -0,0 +1,45 @@
+package algorithm.elice_challenge;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+class 정리정돈을_좋아하는_K씨 {
+    public static void main(String[] args) throws Exception {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        final int m = Integer.parseInt(br.readLine().split(" ")[1]);
+
+        final int[] array = toIntArray(br.readLine().split(" "));
+
+        for (int i = 0; i < m; i++) {
+            final int[] ijk = toIntArray(br.readLine().split(" "));
+            bw.write(Integer.toString(calculate(ijk, array)));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+    private static int[] toIntArray(final String[] array) {
+        return Arrays.stream(array).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static int calculate(final int[] ijk, final int[] target) {
+        final int i = ijk[0];
+        final int j = ijk[1];
+        final int k = ijk[2];
+
+        final int[] array = new int[j - i + 1];
+        int temp = 0;
+
+        for (int index = i - 1; index < j; index++) {
+            array[temp++] = target[index];
+        }
+        Arrays.sort(array);
+
+        return array[k - 1];
+    }
+}
