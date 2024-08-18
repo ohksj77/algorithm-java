2e4245010e15c5847f2f62b9ff3a3170c26eee1d
김승진 <ohksj77@gmail.com>
2024-08-18 21:47:57 +0900

    2024-08-18 알고리즘 문제 풀이 4

---
+++ src/algorithm/codetree/코드트리_사내_메신저.java
@@ -0,0 +1,55 @@
+package algorithm.codetree;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 코드트리_사내_메신저 {
+
+    private static final List<List<Integer>> ADJACENT = new ArrayList<>();
+    private static int n;
+
+    public static void main(String[] args) throws Exception {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        final String[] input = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            ADJACENT.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i < n; i++) {
+            final int boss = Integer.parseInt(input[i]);
+            ADJACENT.get(boss - 1).add(i);
+        }
+
+        bw.write(Integer.toString(dfs(0)));
+        bw.flush();
+    }
+
+    private static int dfs(final int node) {
+        final List<Integer> times = new ArrayList<>();
+        for (final int next : ADJACENT.get(node)) {
+            times.add(dfs(next));
+        }
+
+        if (times.isEmpty()) {
+            return 0;
+        }
+
+        times.sort(Collections.reverseOrder());
+
+        int maxTime = 0;
+        for (int i = 0; i < times.size(); i++) {
+            maxTime = Math.max(maxTime, times.get(i) + (i + 1));
+        }
+
+        return maxTime;
+    }
+}
