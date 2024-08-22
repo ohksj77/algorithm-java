e268f673e03192b0f9fdd674841cf60518e2599d
김승진 <ohksj77@gmail.com>
2024-08-22 15:33:42 +0900

    2024-08-22 알고리즘 문제 풀이 2

--- src/algorithm/baekjoon/Baekjoon1197.java
+++ src/algorithm/baekjoon/Baekjoon1197.java
@@ -4,87 +4,88 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon1197 {
     private static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        final int v = Integer.parseInt(st.nextToken());
+        final int e = Integer.parseInt(st.nextToken());
 
-        List<Node> l = new ArrayList<>();
+        final Queue<Node> nodes = new PriorityQueue<>();
 
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int val = Integer.parseInt(st.nextToken());
+            final int a = Integer.parseInt(st.nextToken());
+            final int b = Integer.parseInt(st.nextToken());
+            final int val = Integer.parseInt(st.nextToken());
 
-            l.add(new Node(A, B, val));
+            nodes.add(new Node(a, b, val));
         }
 
-        Collections.sort(l);
-
         int sum = 0;
-        parent = new int[V + 1];
-        for (int i = 1; i <= V; i++) {
+        parent = new int[v + 1];
+
+        for (int i = 1; i <= v; i++) {
             parent[i] = i;
         }
 
-        for (Node a : l) {
-            int A = a.A;
-            int B = a.B;
-            int val = a.val;
+        while (!nodes.isEmpty()) {
+            final Node node = nodes.poll();
+            final int a = node.a;
+            final int b = node.b;
+            final int val = node.val;
 
-            if (find(A) == find(B)) {
+            if (find(a) == find(b)) {
                 continue;
             }
 
             sum += val;
-            union(A, B);
+            union(a, b);
         }
 
         bw.write(Integer.toString(sum));
         bw.flush();
     }
 
-    private static int find(int A) {
-        if (parent[A] == A) {
-            return A;
+    private static int find(final int a) {
+        if (parent[a] == a) {
+            return a;
         }
-        return find(parent[A]);
+        return find(parent[a]);
     }
 
-    private static void union(int A, int B) {
-        int a = find(A);
-        int b = find(B);
+    private static void union(int a, int b) {
+        final int findA = find(a);
+        final int findB = find(b);
 
-        if (a != b) {
-            parent[b] = a;
+        if (findA != findB) {
+            parent[findB] = findA;
         }
     }
 
     private static class Node implements Comparable<Node> {
-        int A, B, val;
+        private final int a;
+        private final int b;
+        private final int val;
 
-        Node(int A, int B, int val) {
-            this.A = A;
-            this.B = B;
+        public Node(final int a, final int b, final int val) {
+            this.a = a;
+            this.b = b;
             this.val = val;
         }
 
         @Override
-        public int compareTo(Node a) {
-            int val = a.val;
+        public int compareTo(final Node a) {
+            final int val = a.val;
 
             if (this.val > val) {
                 return 1;
