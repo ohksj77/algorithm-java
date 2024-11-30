113e0a416c9ef26d8b92dba0c8de541216980f39
김승진 <ohksj77@gmail.com>
2024-11-30 21:34:11 +0900

    2024-11-30 알고리즘 문제 풀이 2

---
+++ src/algorithm/baekjoon/Baekjoon11382.java
@@ -0,0 +1,22 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon11382 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        long a = Long.parseLong(input[0]);
+        long b = Long.parseLong(input[1]);
+        long c = Long.parseLong(input[2]);
+
+        bw.write(Long.toString(a + b + c));
+        bw.flush();
+    }
+}
