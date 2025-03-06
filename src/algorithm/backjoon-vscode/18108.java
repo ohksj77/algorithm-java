8997cded8039876fd96991f7f155d1eea130c66b
김승진 <ohksj77@gmail.com>
2025-03-06 17:28:54 +0900

    2025-03-06 알고리즘 문제 풀이 1

---
+++ src/algorithm/backjoon-vscode/18108.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int y = Integer.parseInt(br.readLine());
+        bw.write(Integer.toString(y - 543));
+        bw.flush();
+    }
+}
